afe12899228a08fed4aa25f470d35707972fce59
david chong yun hin <1231201533@student.mmu.edu.my>
2025-04-08 23:50:41 +0800

    dd

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "ProductManagement";
+$dbname = "Management";
 
 // 连接数据库
 $conn = new mysqli($servername, $username, $password, $dbname);
