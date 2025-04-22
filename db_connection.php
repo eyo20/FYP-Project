a37d0d08b5df995267b060eef6c31e3a54c255c1
enyong ong <1231203070@student.mmu.edu.my>
2025-04-23 01:05:43 +0800

    new

--- db_connection.php
+++ db_connection.php
@@ -2,7 +2,7 @@
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');  // 
 define('DB_PASSWORD', '');      // 
-define('DB_NAME', 'peer tutoring platform database'); 
+define('DB_NAME', 'peer tutoring platform'); 
 
 
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
