d4709101bedcdf1f0e528d88580404ab5ee5edd6
1231200968_Koh_Meng_Wen <kohmengwen8@gmail.com>
2025-06-03 01:24:15 +0800

    Add files via upload

--- admin_tutors.php
+++ admin_tutors.php
@@ -34,9 +34,7 @@
                 <a href="admin_tutors.php" class="active"><span class="material-symbols-sharp">eyeglasses</span><h3>Tutors</h3></a>
                 <a href="admin_course.php"><span class="material-symbols-sharp">school</span><h3>Courses</h3></a>
                 <a href="message.php"><span class="material-symbols-sharp">chat</span><h3>Messages</h3><span class="message-count">26</span></a>
-                <a href="session.php"><span class="material-symbols-sharp">library_books</span><h3>Session</h3></a>
                 <a href="admin_review.php"><span class="material-symbols-sharp">star</span><h3>Reviews</h3></a>
-                <a href="sales.php"><span class="material-symbols-sharp">finance</span><h3>Sales</h3></a>
                 <a href="home_page.php"><span class="material-symbols-sharp">logout</span><h3>Logout</h3></a>
             </div>
         </aside>
